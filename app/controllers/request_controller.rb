00a97a93afd9e2c2e95a0d75f8aae7c61e16a5e6
Alexander <chaplin1488@gmail.com>
2017-09-08 17:43:05 +0300

    need some fixes, but probably functionality for request from admin done

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -3,6 +3,7 @@ class RequestController < ApplicationController
 
   def index
     @active_request = current_user.requests ## Recieve active requests for current user
+    @request = Request.all
   end
 
   def new
@@ -20,18 +21,31 @@ class RequestController < ApplicationController
     end
   end
 
+  def edit
+    @request = Request.find(params[:id])
+  end
+
   def update
     @request = Request.find(params[:id])
-    if @request.update_attributes(require_params)
-      redirect_to :action => :index
-    else
-      respond_to.html { redirect_to :action => :index, notice: @request.errors}
+    ## When button 'Взять' clicked
+    if @request.allowed == 0
+      unless @request.update_attribute(:allowed, 1) && @request.update_attribute(:admin, current_user.name + ' ' + current_user.surname)
+        render request_index_path, notice: @request.errors
+      else
+        redirect_to request_index_path, notice: 'Заявка успешно принята!'
+      end
+    end
+
+    ## When button 'Выполнено' clicked
+    if @request.allowed == 1 && @request.complited == 0
+      @request.update_attribute(:complited, 1)
+      redirect_to request_index_path, notice: 'Заявка успешно закрыта!'
     end
   end
 
   private
 
   def request_params
-    params.require(:request).permit(:type_of_problem, :message, :allowed, :complited, :admin)
+    params.require(:request).permit(:type_of_problem, :message, :allowed, :complited, :admin, :admin_message)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
 
   get 'request/new'
 
+  get 'request/:id/edit', to: 'request#update', as: 'request_edit'
+
   resources :request
 
   ##Костыль!!!
   post 'request/new', to: 'request#create'
+  post 'request/:id/edit', to: 'request#update', as: 'update_request'
 
   devise_for :users
   get 'persons/profile', as: 'user_root'
